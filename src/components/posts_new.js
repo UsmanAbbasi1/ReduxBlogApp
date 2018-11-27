569a2cfc2026bf0f87773726eb2a5130378f53c3
UsmanAbasi <usman.abasi@hotmail.com>
2018-11-27 20:41:26 +0500

    Add field validation. Show validation errors only when field is touched.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,6 +3,16 @@ import {Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component{
 
+    constructor(props){
+        super(props);
+        this.onSubmit = this.onSubmit.bind(this);
+
+    }
+
+    onSubmit(values){
+        console.log(values);
+    }
+
     renderField(field){
 
         return(
@@ -11,7 +21,10 @@ class PostsNew extends Component{
                 <input
                     className="form-control"
                     type="text"
+                    {...field.input}
                 />
+                /*field has three states i.e. 'pristine', 'touched' and 'invalid' */
+                { field.meta.touched? field.meta.error : ''}
             </div>
 
         );
@@ -19,28 +32,62 @@ class PostsNew extends Component{
     }
 
     render(){
+
+        // Both ways are correct,
+        // second one is shortcut and valid because variable name we are using and property name in this.props is same
+        // const handleSubmit = this.props.handleSubmit;
+        // This data is added in props by reduxForm we attached at bottom of this file
+        const {handleSubmit} = this.props;
+
         return(
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit)}>
                 <Field
                     label="Title"
                     name="title"
+                    /*Field component first run validate() and then call whatever is assigned to component prop*/
                     component={this.renderField}
                 />
                 <Field
-                    label="Tag"
-                    name="tag"
+                    label="Categories"
+                    name="categories"
                     component={this.renderField}
                 />
                 <Field
                     label="Content"
-                    name="Content"
+                    name="content"
                     component={this.renderField}
                 />
+                <button type="submit" className='btn btn-primary'>Submit</button>
             </form>
         );
     }
 }
 
+function validate(values){
+    console.log('val',values.content);
+    let errors = {};
+
+    if (!values.title){
+        errors.title = "Enter some value"
+    }
+    if (!values.categories){
+        errors.categories = "Enter a category"
+    }
+
+    if (!values.content){
+        errors.content = "Enter some content"
+    }
+    else if(values.content.length < 2)
+    {
+        errors.content = "Content length should be atleast 3 characters"
+    }
+
+    // If errors is empty, form is okay to submit otherwise
+    // If errors has any properties, redux form assumes it as invalid
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
 })(PostsNew);
